2ea562825267e1bb1d8d87171aa09eb2e529a9bf
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2022-07-23 16:56:27 +0300

    - References to required libraries are updated

--- application/preview-cs.vs/resource/package/VSPackage.cs
+++ application/preview-cs.vs/resource/package/VSPackage.cs
@@ -24,7 +24,7 @@ namespace resource.package
             public const string DESCRIPTION = "Quick preview of CS files";
             public const string GUID = "5F8DF008-CD59-49F9-91C1-7A6FD770FDF2";
             public const string NAME = "Preview-CS";
-            public const string VERSION = "1.0.12";
+            public const string VERSION = "1.0.13";
         }
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
